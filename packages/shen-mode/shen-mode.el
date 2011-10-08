5f452f481982fd55c183f857ec1311f44a090488
Eric Schulte <schulte.eric@gmail.com>
2011-10-08 14:07:28 -0600

    2011-10-08 Eric Schulte <schulte.eric@gmail.com>
    * shen-mode.el: More complete commentary introducing shen-mode.

--- packages/shen-mode/shen-mode.el
+++ packages/shen-mode/shen-mode.el
@@ -24,7 +24,11 @@
 
 ;;; Commentary:
 
-;; A minor mode for editing shen source code.
+;; A minor mode for editing shen source code.  Shen is a modern lisp
+;; dialect with support for functional and declarative programming,
+;; pattern matching and a very powerful type system.  See the
+;; following for more information on Shen.
+;; http://www.lambdassociates.org/specification/shen_1.8.htm
 
 ;;; Code:
 (require 'lisp-mode)
