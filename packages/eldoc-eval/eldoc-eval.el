bf3fe75dc0f6cee4487acbd03a2f920346afba8b
thierry volpiatto <thierry.volpiatto@gmail.com>
2011-10-15 11:02:09 +0200

    * eldoc-eval.el Add infos in Commentary section.

--- packages/eldoc-eval/eldoc-eval.el
+++ packages/eldoc-eval/eldoc-eval.el
@@ -1,4 +1,4 @@
-;;; eldoc-eval.el --- Show eldoc when using M-:
+;;; eldoc-eval.el --- Enable eldoc support when minibuffer is in use.
 
 ;; Copyright (C) 2011 Free Software Foundation, Inc.
 
@@ -20,6 +20,26 @@
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+;;
+;; This package enable eldoc support when minibuffer is in use.
+;;
+;; Eldoc info are shown by default in mode-line.
+;; but you can have eldoc info somewhere else by setting
+;; `eldoc-in-minibuffer-show-fn' to an other function (e.g `tooltip-show').
+;;
+;; By default with this package `M-:' will use `pp-eval-expression'
+;; instead of `eval-expression'; You can change that in
+;; `eval-prefered-function'.
+;;
+;; It provide also a convenient macro to enable eldoc support
+;; in your own functions using minibuffer or in your defadvices,
+;; that is `with-eldoc-in-minibuffer'.
+;;
+;; Users of own minibuffer frame will have to set
+;; `eldoc-in-minibuffer-own-frame-p' to non--nil.
+;;
+;; You can turn off at anytime eldoc support in minibuffer
+;; by setting `eldoc-in-minibuffer' to nil.
 
 ;;; Code:
 (require 'eldoc)
