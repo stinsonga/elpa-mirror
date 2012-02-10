79b21cd6fa9eb345fd6a82584a4580c5c252e964
Christopher Schmidt <christopher@ch.ristopher.com>
2012-02-10 23:22:50 +0100

    ampc is (hopefully) a part of GNU Emacs.

--- ampc.el
+++ ampc.el
@@ -9,7 +9,7 @@
 ;; Keywords: mpc
 ;; Compatibility: GNU Emacs: 24.x
 
-;; This file is NOT part of GNU Emacs.
+;; This file is part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
