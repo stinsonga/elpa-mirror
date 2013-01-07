421c0e4bf686aa65f1c29e2c6176c06fcbd46ef7
Michael Albinus <michael.albinus@gmx.de>
2013-01-07 16:26:31 +0100

    Adapt copyright years.
    * debbugs-gnu.el (debbugs-gnu-default-packages): Extend list of packages.
    (debbugs-gnu-get-bugs): Support 'bugs query.
    (debbugs-gnu-usertags): Simplify implementation.
    (debbugs-gnu-bugs): New defun.

--- packages/debbugs/debbugs.el
+++ packages/debbugs/debbugs.el
@@ -1,6 +1,6 @@
 ;;; debbugs.el --- SOAP library to access debbugs servers
 
-;; Copyright (C) 2011, 2012 Free Software Foundation, Inc.
+;; Copyright (C) 2011-2013 Free Software Foundation, Inc.
 
 ;; Author: Michael Albinus <michael.albinus@gmx.de>
 ;; Keywords: comm, hypermedia
