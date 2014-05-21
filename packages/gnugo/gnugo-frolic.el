74e2b09aed04988bcec9180c29f1b6afb270d937
Thien-Thi Nguyen <ttn@gnu.org>
2014-05-21 13:28:35 +0200

    [gnugo maint] Add some headers to gnugo-frolic.el; nfc.

--- packages/gnugo/gnugo-frolic.el
+++ packages/gnugo/gnugo-frolic.el
@@ -2,6 +2,9 @@
 
 ;; Copyright (C) 2014  Free Software Foundation, Inc.
 
+;; Author: Thien-Thi Nguyen <ttn@gnu.org>
+;; Maintainer: Thien-Thi Nguyen <ttn@gnu.org>
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
