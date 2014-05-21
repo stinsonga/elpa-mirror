701e1c509b58e8232fd2085f73165cec6b6960fd
Thien-Thi Nguyen <ttn@gnu.org>
2014-05-21 09:26:01 +0200

    [xpm] Release: 1.0.1
    * packages/xpm/xpm.el [Version]: Bump to "1.0.1".

--- packages/xpm/xpm.el
+++ packages/xpm/xpm.el
@@ -4,7 +4,7 @@
 
 ;; Author: Thien-Thi Nguyen <ttn@gnu.org>
 ;; Maintainer: Thien-Thi Nguyen <ttn@gnu.org>
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Keywords: multimedia, xpm
 
 ;; This program is free software; you can redistribute it and/or modify
