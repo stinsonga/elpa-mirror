fd7b3af31918d74146cb8d0a8406442e566ae690
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2015-06-11 21:02:40 +0200

    Added ztree package

---
+++ packages/ztree/ztree-pkg.el
@@ -0,0 +1,4 @@
+;;; ztree-pkg.el --- Package file for MELPA/ELPA
+(define-package "ztree" "1.0.1"  "Several text-tree applications" :url "https://github.com/fourier/ztree" :keywords '("files" "tools"))
+
+;;; ztree-pkg.el ends here
