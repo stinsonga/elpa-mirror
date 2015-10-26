cec498235ef5601ac054d5df5533366e4e0d4f80
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2015-10-25 23:49:42 -0400

    url-http-ntlm.el: Update copyright owner and years

--- packages/url-http-ntlm/url-http-ntlm.el
+++ packages/url-http-ntlm/url-http-ntlm.el
@@ -1,6 +1,6 @@
 ;;; url-http-ntlm.el --- NTLM authentication for the url library
 
-;; Copyright (C) 2008  Tom Schutzer-Weissmann
+;; Copyright (C) 2008, 2015 Free Software Foundation, Inc.
 
 ;; Author: Tom Schutzer-Weissmann <tom@schutzer-weissmann.net>
 ;; Keywords: comm, data, processes, hypermedia
