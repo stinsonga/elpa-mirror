e84487c17bcfa432e59e616449e22397f5d1fdbf
Daiki Ueno <ueno@gnu.org>
2016-01-19 15:34:55 +0900

    snippet: Add FIXME comment

--- gnome-c-snippet.el
+++ gnome-c-snippet.el
@@ -20,6 +20,10 @@
 ;; along with this program.  If not, see
 ;; <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; FIXME: The snippets defined here could be rewritten in yasnippet
+
 ;;; Code:
 
 (require 'gnome-c-align)
