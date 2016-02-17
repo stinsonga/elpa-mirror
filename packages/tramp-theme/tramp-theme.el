868cc3d8b2a59ebd7bbdf68e16e68f57a863c0fd
Michael Albinus <michael.albinus@gmx.de>
2016-02-17 11:59:49 +0100

    Oops, release tramp-theme 0.1.1
    * packages/tramp-theme/README:
    * packages/tramp-theme/tramp-theme.el: New files.

--- packages/tramp-theme/tramp-theme.el
+++ packages/tramp-theme/tramp-theme.el
@@ -3,19 +3,19 @@
 ;; Copyright (C) 2016 Free Software Foundation, Inc.
 
 ;; Author: Michael Albinus <michael.albinus@gmx.de>
+;; Keywords: convenience, faces
 ;; Package: tramp-theme
-;; Version: 0.1
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "24.1"))
 
+;; This file is not part of GNU Emacs.
 
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software: you can redistribute it and/or modify
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
