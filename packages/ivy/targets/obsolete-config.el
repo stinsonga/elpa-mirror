45752a93be63c9fc06129fc9826944db9697d4d9
Stefan Monnier <monnier@iro.umontreal.ca>
2017-04-10 13:29:58 -0400

    ivy/targets/obsolete-config.el: Satisfy check_copyright

--- packages/ivy/targets/obsolete-config.el
+++ packages/ivy/targets/obsolete-config.el
@@ -1,3 +1,5 @@
+;; Copyright (C) 2017  Free Software Foundation, Inc.
+
 (add-to-list 'load-path default-directory)
 (require 'counsel)
 (setq counsel-prompt-function 'counsel-prompt-function-default)
