f213b14fb91b98b57269a2f442d14fe03f45c531
Ian Dunn <dunni@gnu.org>
2017-04-27 20:52:37 -0400

    Backported copyright fix from ELPA

--- hook-helpers-tests.el
+++ hook-helpers-tests.el
@@ -1,7 +1,6 @@
 ;;; hook-helpers-tests.el --- Tests for hook helpers
 
-;; Copyright (C) 2016 Ian Dunn
-;; Copyright (C) 2017 Free Software Foundation, Inc.
+;; Copyright (C) 2016-2017 Free Software Foundation, Inc.
 
 ;; Author: Ian Dunn <dunni@gnu.org>
 ;; Keywords: development, hooks
