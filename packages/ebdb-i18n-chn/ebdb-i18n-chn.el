2da92be1fc52060950927228bf3a63e0fb6ae259
Stefan Monnier <monnier@iro.umontreal.ca>
2017-08-15 18:20:51 -0400

    * edbd-i18n-chn.el: Fix typo in metadata

--- packages/ebdb-i18n-chn/ebdb-i18n-chn.el
+++ packages/ebdb-i18n-chn/ebdb-i18n-chn.el
@@ -5,7 +5,7 @@
 ;; Author: Eric Abrahamsen <eric@ericabrahamsen.net>
 ;; Maintainer: Eric Abrahamsen <eric@ericabrahamsen.net>
 ;; Version: 1
-;; Package-Requires: ((pyim "1.6.0") (ebdb "0.2))
+;; Package-Requires: ((pyim "1.6.0") (ebdb "0.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
