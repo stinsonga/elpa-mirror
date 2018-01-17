4523fbb41b9c650cf02353eaca3e0dbb6d1246de
Michael Albinus <michael.albinus@gmx.de>
2018-01-17 16:24:06 +0100

    Bump debbugs 0.15
    	* packages/debbugs/Debbugs.wsdl:
    	* packages/debbugs/debbugs.el:
    	* packages/debbugs/debbugs-browse.el:
    	* packages/debbugs/debbugs-gnu.el:
    	* packages/debbugs/debbugs-org.el:
    	* packages/debbugs/debbugs-ug.texi:
    	* packages/debbugs/debbugs.texi: Update copyright years.
    
    	* packages/debbugs/debbugs-ug.texi (Minor Mode):
    	Mention `debbugs-browse-url-regexp'.
    
    	* packages/debbugs/debbugs.el: Increase Version to 0.15.

--- packages/debbugs/debbugs-gnu.el
+++ packages/debbugs/debbugs-gnu.el
@@ -1,6 +1,6 @@
 ;;; debbugs-gnu.el --- interface for the GNU bug tracker  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2011-2017 Free Software Foundation, Inc.
+;; Copyright (C) 2011-2018 Free Software Foundation, Inc.
 
 ;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
 ;;         Michael Albinus <michael.albinus@gmx.de>
