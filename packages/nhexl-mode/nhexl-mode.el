fc8ae0ec25e28a3429d5c798b4d87ac87780759d
Stefan Monnier <monnier@iro.umontreal.ca>
2018-04-15 15:32:08 -0400

    * nhexl-mode.el: Bump version number for new release

--- packages/nhexl-mode/nhexl-mode.el
+++ packages/nhexl-mode/nhexl-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Stefan Monnier <monnier@iro.umontreal.ca>
 ;; Keywords: data
-;; Version: 0.3
+;; Version: 0.4
 ;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
