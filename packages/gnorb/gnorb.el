0786fbc58af64e5232041941883258f258eea313
Eric Abrahamsen <eric@ericabrahamsen.net>
2018-05-04 11:36:02 -0700

    [gnorb] Bump version to 1.5.3

--- packages/gnorb/gnorb.el
+++ packages/gnorb/gnorb.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2018  Free Software Foundation, Inc.
 
-;; Version: 1.5.2
+;; Version: 1.5.3
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; Maintainer: Eric Abrahamsen <eric@ericabrahamsen.net>
