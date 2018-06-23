9802a197c3fd2515c19240488a257a79ec9bc312
Michael Heerdegen <michael_heerdegen@web.de>
2018-06-23 18:38:40 +0200

    [el-search] Bump version to 1.7.1

--- packages/el-search/el-search.el
+++ packages/el-search/el-search.el
@@ -7,7 +7,7 @@
 ;; Created: 29 Jul 2015
 ;; Keywords: lisp
 ;; Compatibility: GNU Emacs 25
-;; Version: 1.7
+;; Version: 1.7.1
 ;; Package-Requires: ((emacs "25") (stream "2.2.4") (cl-print "1.0"))
 
 
