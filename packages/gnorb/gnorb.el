32d058ac763b946677b492380b5722927c6d682c
Eric Abrahamsen <eric@ericabrahamsen.net>
2018-09-29 12:30:58 -0700

    gnorb: Fix link following, bump to 1.5.6
    * packages/gnorb/gnorb-utils.el (gnorb-open-gnus-link): Remove
      leftover article number caching: you're explicitly not supposed to
      do this with the registry (article numbers are subject to change)
      and it was causing problems.
    * packages/gnorb/gnorb.el: Bump and flush.

--- packages/gnorb/gnorb.el
+++ packages/gnorb/gnorb.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2018  Free Software Foundation, Inc.
 
-;; Version: 1.5.5
+;; Version: 1.5.6
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; Maintainer: Eric Abrahamsen <eric@ericabrahamsen.net>
