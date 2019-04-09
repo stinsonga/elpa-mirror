a82e88c4214e0deda189e771a6b6aac79f95f845
Eric Abrahamsen <eric@ericabrahamsen.net>
2019-04-09 14:27:56 -0700

    Add nnml server with non-ASCII group name, bump to 0.4.1
    In aftermath of the whole Gnus hashtables mess.
    
    * packages/gnus-mock/data/.newsrc.eld: Add group.
    * packages/gnus-mock/data/active:
    * packages/gnus-mock/data/テスト/.overview:
    * packages/gnus-mock/data/テスト/1: Data for group.
    * packages/gnus-mock/gnus-mock.el: Bump version.

--- packages/gnus-mock/gnus-mock.el
+++ packages/gnus-mock/gnus-mock.el
@@ -5,7 +5,7 @@
 ;; Author: Eric Abrahamsen <eric@ericabrahamsen.net>
 ;; Maintainer: Eric Abrahamsen <eric@ericabrahamsen.net>
 ;; Package-Type: multi
-;; Version: 0.4.0
+;; Version: 0.4.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
