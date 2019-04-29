b5774453d49942e57434686a2b0f140f8ae3abbc
Stefan Monnier <monnier@iro.umontreal.ca>
2019-04-29 17:16:14 -0400

    * gnu-elpa-keyring-update.el: Bump version

--- packages/gnu-elpa-keyring-update/gnu-elpa-keyring-update.el
+++ packages/gnu-elpa-keyring-update/gnu-elpa-keyring-update.el
@@ -5,7 +5,7 @@
 ;; Author: Stefan Monnier <monnier@iro.umontreal.ca>
 ;; Keywords: maint, tools
 ;; Package-Type: multi
-;; Version: 2019.2
+;; Version: 2019.3
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
