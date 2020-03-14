334cd89138235acae1901a0d7c9581f997a500df
Eric Abrahamsen <eric@ericabrahamsen.net>
2020-03-14 13:01:56 -0700

    [vcard] Use vars, not options, for altering parsing process
    * packages/vcard/vcard-parse.el (vcard-parse-select-fields,
    vcard-parse-omit-fields, vcard-parse-datetime-values,
    vcard-parse-card-consumer-function,
    vcard-parse-property-consumer-function): None of these make sense as
    defcustoms, use defvar instead. This lets us get rid of the
    parsing customization group altogether.

--- packages/vcard/vcard-parse.el
+++ packages/vcard/vcard-parse.el
@@ -92,53 +92,43 @@
 
 ;;; Code:
 
-(require 'vcard)
 (require 'cl-lib)
 (require 'iso8601)
 
-(defgroup vcard-parse nil
-  "Customization options for vcard parsing."
-  :group 'vcard)
-
-(defcustom vcard-parse-select-fields nil
+(defvar vcard-parse-select-fields nil
   "A list of field types to select.
 If this variable is non-nil, only the fields listed will be
 parsed, all others will be discarded.  Note that the 'version and
 'fn properties are always returned.
 
-Most useful when let-bound around one of the parsing functions."
-  :type '(repeat symbol))
+Most useful when let-bound around one of the parsing functions.")
 
-(defcustom vcard-parse-omit-fields nil
+(defvar vcard-parse-omit-fields nil
   "A list of field types to omit.
 If this variable is non-nil, the fields listed will be discarded.
 
-Most useful when let-bound around one of the parsing functions."
-  :type '(repeat symbol))
+Most useful when let-bound around one of the parsing functions.")
 
-(defcustom vcard-parse-datetime-values t
+(defvar vcard-parse-datetime-values t
   "When non-nil, attempt to parse date/time property values.
 If successful, the property value will be (usually) converted to
 a list of integers, though if the \"type\" parameter of the
 property is \"text\", the value will be returned as a string.  It
 is also possible that parsing may fail, in which case the
-original string value will also be returned."
-  :type 'boolean)
+original string value will also be returned.")
 
-(defcustom vcard-parse-card-consumer-function nil
+(defvar vcard-parse-card-consumer-function nil
   "Custom function for consuming a single contact card.
 It is called with a list of properties, as produced by the
 built-in code, or by the return value of
-`vcard-parse-property-consumer-function'."
-  :type 'function)
+`vcard-parse-property-consumer-function'.")
 
-(defcustom vcard-parse-property-consumer-function nil
+(defvar vcard-parse-property-consumer-function nil
   "Custom function for consuming a single property.
 The function is called with four arguments: the property type as
 a symbol, the property value (all un-escaping, decoding,
 splitting, etc already complete), the property parameters as an
-alist with symbol keys, and the vcard version as a float."
-  :type 'function)
+alist with symbol keys, and the vcard version as a float.")
 
 (defvar vcard-parse-overriding-version nil
   "vCard version, as a float, used when no VERSION property is present.
