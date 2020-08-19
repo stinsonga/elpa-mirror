42e7309b05c6323288c5ceca88035a49f69f87e7
Phil Sainty <psainty@orcon.net.nz>
2020-08-19 16:18:58 +0200

    Make vlf handle nil large-file-warning-threshold value
    * packages/vlf/vlf-tune.el (vlf-tune-max): Handle a nil value of
    'large-file-warning-threshold' (bug#42930).

--- packages/vlf/vlf-tune.el
+++ packages/vlf/vlf-tune.el
@@ -60,7 +60,9 @@ but don't change batch size.  If t, measure and change."
                                (if ram-size
                                    (/ ram-size 20)
                                  0))
-                             large-file-warning-threshold)
+                             (or large-file-warning-threshold
+                                 (eval (car (get 'large-file-warning-threshold
+                                                 'standard-value)))))
   "Maximum batch size in bytes when auto tuning.
 Avoid increasing this after opening file with VLF."
   :group 'vlf :type 'integer)
