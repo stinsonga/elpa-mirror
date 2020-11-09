8675c0f8a10dfd6601f05d4f3c97b5086057059c
Simen Heggestøyl <simenheg@runbox.com>
2020-11-09 17:44:29 +0100

    Bump json-mode version to 0.2

--- packages/json-mode/json-mode.el
+++ packages/json-mode/json-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Simen Heggestøyl <simenheg@gmail.com>
 ;; Maintainer: Simen Heggestøyl <simenheg@gmail.com>
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: data
 
